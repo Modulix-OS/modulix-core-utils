8305aaaf82d66a0aaf41279ab7728e4dc9cbd37c
qhorgues <quentin.horgues@outlook.fr>
2026-02-08 17:21:33 +0100

    add function for remove option (set to default value)

--- examples/test.rs
+++ examples/test.rs
@@ -1,6 +1,7 @@
-use modulix_core_utils::edit_option::set_option;
+use modulix_core_utils::edit_option::{set_option, set_option_to_default};
 
 
 fn main() {
     set_option("./test.nix", "test.ni.enable", "./nix/temp").unwrap();
+    set_option_to_default("./test.nix", "test.ni.enable").unwrap();
 }
--- src/edit_option.rs
+++ src/edit_option.rs
@@ -3,18 +3,21 @@ use rnix::{TextRange, TextSize};
 
 use crate::{core::localise_option::SettingsPosition, core::write_file};
 
-pub fn set_option(nix_file_path: &str, nix_option: &str, option_value: &str) -> Result<(), String>{
+fn pos_option_in_file<'a>(file_content: &str, nix_option: &'a str) -> Result<SettingsPosition<'a>, String> {
+    let ast = rnix::Root::parse(&file_content);
+    match SettingsPosition::new(&ast.syntax(), nix_option) {
+        Some(p) => Ok(p),
+        None => return Err(String::from("Impossible to set option in file")),
+    }
+}
 
+pub fn set_option(nix_file_path: &str, nix_option: &str, option_value: &str) -> Result<(), String>{
     let mut file_content = match fs::read_to_string(nix_file_path) {
         Ok(c) => c,
         Err(e) => return Err(e.to_string()),
     };
-    let ast = rnix::Root::parse(&file_content);
 
-    let pos = match SettingsPosition::new(&ast.syntax(), nix_option) {
-        Some(p) => p,
-        None => return Err(String::from("Impossible to set option in file")),
-    };
+    let pos = pos_option_in_file(&file_content, nix_option)?;
 
     if let Some(path) = pos.get_remaining_path() {
         let indent = if pos.get_indent_level() > 0u8 {
@@ -42,4 +45,29 @@ pub fn set_option(nix_file_path: &str, nix_option: &str, option_value: &str) -> 
     }
     write_file::write_file(nix_file_path, file_content.as_str())?;
     return Ok(());
+}
+
+// Delete option in file
+pub fn set_option_to_default(nix_file_path: &str, nix_option: &str) -> Result<bool, String> {
+    let mut file_content = match fs::read_to_string(nix_file_path) {
+        Ok(c) => c,
+        Err(e) => return Err(e.to_string()),
+    };
+
+    let pos = pos_option_in_file(&file_content, nix_option)?;
+
+    if let Some(_) = pos.get_pos_definition_value() {
+        file_content.replace_range(<TextRange as Into<Range<usize>>>::into( pos.get_pos_definition()), "");
+        let pos = <TextSize as Into<usize>>::into(pos.get_pos_definition().start())-1usize;
+        while match file_content.chars().nth(pos) {
+            Some(' ') | Some('\t') | Some('\n') => true,
+            Some(_) | _ => false,
+        } {
+            file_content.remove(pos);
+        }
+        write_file::write_file(nix_file_path, file_content.as_str())?;
+        Ok(true)
+    } else {
+        Ok(false)
+    }
 }
