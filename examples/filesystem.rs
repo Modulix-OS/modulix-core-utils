e3055591cf6f194dc98831f79989038c248d7ca6
qhorgues <quentin.horgues@outlook.fr>
2026-03-02 00:54:50 +0100

    adapt exemple for new function

--- examples/filesystem.rs
+++ examples/filesystem.rs
@@ -5,17 +5,9 @@ fn main() {
         "/mnt/Games",
         "/dev/disk/by-uuid/1b35568b-4447-4c80-9880-4b359d4ecb6c",
         "ext4",
-        &["noatime", "nodiratime", "discard", "defaults", "commit=120"],
+        &[],
         false,
     )
     .unwrap();
-
-    filesystem::add_entry(
-        "/",
-        "/dev/disk/by-uuid/208b9468-df96-4f4a-b381-3275e42a77c6",
-        "ext4",
-        &["noatime", "nodiratime", "discard", "defaults", "commit=120"],
-        true,
-    )
-    .unwrap();
+    filesystem::remove_entry("/mnt/Games").unwrap();
 }
--- examples/user.rs
+++ examples/user.rs
@@ -1,48 +1,14 @@
-use modulix_core_utils::mx::core::{
-    list::List, option::Option, transaction::Transaction, transaction::transaction::BuildCommand,
-};
+use modulix_core_utils::user;
 
 fn main() {
-    let mut transac_add_pkgs = Transaction::new("Create qhor User", BuildCommand::Switch).unwrap();
-    transac_add_pkgs
-        .add_file("/home/quentin/Programmes/Modulix-OS/modulix-core-utils/test/configuration.nix")
-        .unwrap();
-
-    transac_add_pkgs.begin().unwrap();
-
-    let file = transac_add_pkgs
-        .get_file("/home/quentin/Programmes/Modulix-OS/modulix-core-utils/test/configuration.nix")
-        .unwrap();
-
-    let user = Option::new("users.users.qhor.isNormalUser");
-    user.set(file, "true").unwrap();
-
-    let user_password = Option::new("users.users.qhor.initialPassword");
-    user_password.set(file, "\"1234\"").unwrap();
-
-    let user_home = Option::new("users.users.qhor.createHome");
-    user_home.set(file, "true").unwrap();
-
-    let user_home = Option::new("users.users.qhor.createHome");
-    user_home.set(file, "true").unwrap();
-
-    let user_group = Option::new("users.users.qhor.group");
-    user_group.set(file, "\"users\"").unwrap();
-
-    let extra_group = List::new("users.users.qhor.extraGroups", true);
-    extra_group.add(file, "\"wheel\"").unwrap();
-
-    let user_descrition = Option::new("users.users.qhor.description");
-    user_descrition.set(file, "\'\'Main user\'\'").unwrap();
-
-    let user_shell = Option::new("users.users.qhor.shell");
-    user_shell.set(file, "\"${pkgs.bash}/bin/bash\"").unwrap();
-
-    match transac_add_pkgs.commit() {
-        Ok(_) => (),
-        Err(e) => println!("{}", e.to_string()),
-    };
-
-    // let mut f = fs::File::create("./test.txt").unwrap();
-    // f.write("Coucou".as_bytes()).unwrap();
+    // user::add(
+    //     "modulix",
+    //     "1234",
+    //     "Modulix OS",
+    //     "${pkgs.zsh}/bin/zsh",
+    //     &["wheel"],
+    //     true,
+    // )
+    // .unwrap();
+    user::remove("modulix").unwrap();
 }
