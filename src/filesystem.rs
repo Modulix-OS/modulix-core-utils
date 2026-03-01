063cda52dcd651f6cbe8b99fbda6cd1f55de8a47
qhorgues <quentin.horgues@outlook.fr>
2026-03-02 00:45:44 +0100

    manage exception for filesystem and add option to set on unix fstab

--- src/filesystem.rs
+++ src/filesystem.rs
@@ -1,3 +1,5 @@
+use std::process;
+
 use crate::{
     core::{
         list::List as mxList,
@@ -25,35 +27,88 @@ pub fn add_entry(
     filesystem_transaction.add_file(FILE_SYSTEM_PATH)?;
     filesystem_transaction.begin()?;
 
-    let fstab = filesystem_transaction.get_file(FILE_SYSTEM_PATH)?;
+    let fstab = match filesystem_transaction.get_file(FILE_SYSTEM_PATH) {
+        Ok(f) => f,
+        Err(e) => {
+            filesystem_transaction.rollback()?;
+            return Err(e);
+        }
+    };
 
     if encrypted {
-        let uuid = device
+        let uuid = match device
             .strip_prefix("/dev/disk/by-uuid/")
-            .ok_or(mx::ErrorKind::InvalidUuid)?;
+            .ok_or(mx::ErrorKind::InvalidUuid)
+        {
+            Ok(uuid) => uuid,
+            Err(e) => {
+                filesystem_transaction.rollback()?;
+                return Err(e);
+            }
+        };
         let luks_name = format!("luks-{}", uuid);
         let luks_path = format!("/dev/mapper/{}", luks_name);
         let luks_option = format!("boot.initrd.luks.devices.\"{}\"", luks_name);
-        mxOption::new(&format!("{}.device", luks_option))
-            .set(fstab, format!("\"{}\"", device).as_str())?;
+        match mxOption::new(&format!("{}.device", luks_option))
+            .set(fstab, format!("\"{}\"", device).as_str())
+        {
+            Ok(_) => (),
+            Err(e) => {
+                filesystem_transaction.rollback()?;
+                return Err(e);
+            }
+        };
 
-        mxOption::new(format!("{}.device", root_option).as_str())
-            .set(fstab, format!("\"{}\"", luks_path).as_str())?;
+        match mxOption::new(format!("{}.device", root_option).as_str())
+            .set(fstab, format!("\"{}\"", luks_path).as_str())
+        {
+            Ok(()) => (),
+            Err(e) => {
+                filesystem_transaction.rollback()?;
+                return Err(e);
+            }
+        };
     } else {
-        mxOption::new(format!("{}.device", root_option).as_str())
-            .set(fstab, format!("\"{}\"", device).as_str())?;
+        match mxOption::new(format!("{}.device", root_option).as_str())
+            .set(fstab, format!("\"{}\"", device).as_str())
+        {
+            Ok(()) => (),
+            Err(e) => {
+                filesystem_transaction.rollback()?;
+                return Err(e);
+            }
+        };
     }
 
-    mxOption::new(format!("{}.fsType", root_option).as_str())
-        .set(fstab, format!("\"{}\"", fs_type).as_str())?;
+    match mxOption::new(format!("{}.fsType", root_option).as_str())
+        .set(fstab, format!("\"{}\"", fs_type).as_str())
+    {
+        Ok(()) => (),
+        Err(e) => {
+            filesystem_transaction.rollback()?;
+            return Err(e);
+        }
+    };
 
     let option_path = format!("{}.options", root_option);
 
-    mxOption::new(&option_path).set_option_to_default(fstab)?;
+    match mxOption::new(&option_path).set_option_to_default(fstab) {
+        Ok(_) => (),
+        Err(e) => {
+            filesystem_transaction.rollback()?;
+            return Err(e);
+        }
+    };
 
     let list_opt = mxList::new(&option_path, true);
     for o in option {
-        list_opt.add(fstab, &format!("\"{}\"", o))?;
+        match list_opt.add(fstab, &format!("\"{}\"", o)) {
+            Ok(()) => (),
+            Err(e) => {
+                filesystem_transaction.rollback()?;
+                return Err(e);
+            }
+        };
     }
 
     filesystem_transaction.commit()?;
@@ -71,14 +126,21 @@ pub fn remove_entry(mount_point: &str) -> mx::Result<bool> {
     filesystem_transaction.add_file(FILE_SYSTEM_PATH)?;
     filesystem_transaction.begin()?;
 
-    let fstab = filesystem_transaction.get_file(FILE_SYSTEM_PATH)?;
+    let fstab = match filesystem_transaction.get_file(FILE_SYSTEM_PATH) {
+        Ok(f) => f,
+        Err(e) => {
+            filesystem_transaction.rollback()?;
+            return Err(e);
+        }
+    };
 
-    let found = mxOption::new(&format!("{}.device", root_option)).set_option_to_default(fstab)?;
-
-    if found {
-        mxOption::new(format!("{}.fsType", root_option).as_str()).set_option_to_default(fstab)?;
-        mxOption::new(&format!("{}.options", root_option)).set_option_to_default(fstab)?;
-    }
+    let found = match mxOption::new(&root_option).set_option_all_instance_to_default(fstab) {
+        Ok(f) => f,
+        Err(e) => {
+            filesystem_transaction.rollback()?;
+            return Err(e);
+        }
+    };
 
     filesystem_transaction.commit()?;
     Ok(found)
@@ -91,11 +153,23 @@ pub fn add_swap(device: &str) -> mx::Result<()> {
     transaction_swap.add_file(FILE_SYSTEM_PATH)?;
     transaction_swap.begin()?;
 
-    let fstab = transaction_swap.get_file(FILE_SYSTEM_PATH)?;
+    let fstab = match transaction_swap.get_file(FILE_SYSTEM_PATH) {
+        Ok(f) => f,
+        Err(e) => {
+            transaction_swap.rollback()?;
+            return Err(e);
+        }
+    };
 
     let list_swap = mxList::new("swapDevices", true);
     let new_entry = format!("{{device={};}}", device);
-    list_swap.add(fstab, &new_entry)?;
+    match list_swap.add(fstab, &new_entry) {
+        Ok(()) => (),
+        Err(e) => {
+            transaction_swap.rollback()?;
+            return Err(e);
+        }
+    }
 
     transaction_swap.commit()?;
     Ok(())
@@ -110,12 +184,81 @@ pub fn remove_swap(device: &str) -> mx::Result<()> {
     transaction_swap.add_file(FILE_SYSTEM_PATH)?;
     transaction_swap.begin()?;
 
-    let fstab = transaction_swap.get_file(FILE_SYSTEM_PATH)?;
+    let fstab = match transaction_swap.get_file(FILE_SYSTEM_PATH) {
+        Ok(f) => f,
+        Err(e) => {
+            transaction_swap.rollback()?;
+            return Err(e);
+        }
+    };
 
     let list_swap = mxList::new("swapDevices", true);
     let new_entry = format!("{{device={};}}", device);
-    list_swap.remove(fstab, &new_entry)?;
+    match list_swap.remove(fstab, &new_entry) {
+        Ok(()) => (),
+        Err(e) => {
+            transaction_swap.rollback()?;
+            return Err(e);
+        }
+    };
 
     transaction_swap.commit()?;
+    Ok(())
+}
+
+fn get_filesystem_from_fstab() -> mx::Result<String> {
+    let full = process::Command::new("nixos-generate-config")
+        .args(["--show-hardware-config"])
+        .output()
+        .map_err(mx::ErrorKind::IOError)?;
+
+    let no_fs = process::Command::new("nixos-generate-config")
+        .args(["--show-hardware-config", "--no-filesystems"])
+        .output()
+        .map_err(mx::ErrorKind::IOError)?;
+
+    let full_str = String::from_utf8(full.stdout).map_err(|_| mx::ErrorKind::InvalidFile)?;
+    let no_fs_str = String::from_utf8(no_fs.stdout).map_err(|_| mx::ErrorKind::InvalidFile)?;
+
+    let no_fs_lines: std::collections::HashSet<&str> = no_fs_str.lines().collect();
+    let diff: Vec<&str> = full_str
+        .lines()
+        .filter(|line| !no_fs_lines.contains(line))
+        .collect();
+
+    Ok(diff.join("\n"))
+}
+
+pub fn def_filesystem_from_unix_fstab() -> mx::Result<()> {
+    let mut transaction_reset = Transaction::new("Reset filesystem", BuildCommand::Build)?;
+
+    transaction_reset.add_file(FILE_SYSTEM_PATH)?;
+    transaction_reset.begin()?;
+
+    let fstab = match transaction_reset.get_file(FILE_SYSTEM_PATH) {
+        Ok(f) => f,
+        Err(e) => {
+            transaction_reset.rollback()?;
+            return Err(e);
+        }
+    };
+
+    let content: &mut String = match fstab.get_mut_file_content() {
+        Ok(content) => content,
+        Err(e) => {
+            transaction_reset.rollback()?;
+            return Err(e);
+        }
+    };
+
+    let new_file: String = match get_filesystem_from_fstab() {
+        Ok(s) => s,
+        Err(e) => {
+            transaction_reset.rollback()?;
+            return Err(e);
+        }
+    };
+    *content = format!("{{config, lib, pkgs, ...}}:\n{{\n{}\n}}\n", new_file);
+    transaction_reset.commit()?;
     Ok(())
 }
