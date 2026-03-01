d4f8bf4f2285281d9c7d51f28c97f29418140977
qhorgues <quentin.horgues@outlook.fr>
2026-03-01 13:31:45 +0100

    add constant for repo pos

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,16 @@
+#[cfg(debug_assertions)]
+use const_format::concatcp;
+
 pub mod core; // TODO: Swap to private
 mod error;
+pub mod filesystem;
+
+#[cfg(not(debug_assertions))]
+const CONFIG_DIRECTORY: &str = "/etc/nixos/";
+#[cfg(debug_assertions)]
+const CONFIG_DIRECTORY: &str = concatcp!(env!("CARGO_MANIFEST_DIR"), "/test/");
+
+const CONFIG_NAME: &str = "default";
 
 pub mod mx {
     pub use crate::core;
